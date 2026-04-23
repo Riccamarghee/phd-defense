fec3c948e5917b1bf179001d3198e1f7cae0af3b
Riccardo Margheritti <riccardo.margheritti@mail.polimi.it>
2026-04-23 10:55:37 +0200

    configured css and js

---
+++ js/config.js
@@ -0,0 +1,9 @@
+Reveal.initialize({
+  hash: true,
+  slideNumber: true,
+  controls: true,
+  progress: true,
+  center: true,
+  transition: 'slide',
+  plugins: [ RevealMath.KaTeX, RevealNotes, RevealHighlight ]
+});
